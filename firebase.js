3f3877404084c8aa037c06d6804e24d6fbf4aa6f
Kian2002 <canadakurdistan@gmail.com>
2023-01-11 01:56:44 -0800

    deployment fixes

--- firebase.js
+++ firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: JSON.stringify(import.meta.env.VITE_API_KEY),
-  authDomain: JSON.stringify(import.meta.env.VITE_AUTH_DOMAIN),
-  projectId: JSON.stringify(import.meta.env.VITE_PROJECT_ID),
-  storageBucket: JSON.stringify(import.meta.env.VITE_STORAGE_BUCKET),
-  messagingSenderId: JSON.stringify(import.meta.env.VITE_MESSAGING_SENDER_ID),
-  appId: JSON.stringify(import.meta.env.VITE_APP_ID),
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-const URL = JSON.stringify(import.meta.env.VITE_URL);
+const URL = import.meta.env.VITE_URL;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
